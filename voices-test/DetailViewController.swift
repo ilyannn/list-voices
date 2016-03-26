cfdfd36e08eeaa1bc5d54915c6a94c80751a7bc0
ilya n <ilya.nikokoshev@gmail.com>
2016-03-26 15:09:08 +0100

    Add search

--- voices-test/DetailViewController.swift
+++ voices-test/DetailViewController.swift
@@ -10,63 +10,63 @@ import UIKit
 import AVFoundation
 
 class DetailViewController: UIViewController {
-
-  @IBOutlet weak var detailDescriptionLabel: UILabel!
-  @IBOutlet weak var languageIdentifierLabel: UILabel?
-
-  @IBOutlet weak var languageNameLabel: UILabel!
-  @IBOutlet weak var countryNameLabel: UILabel!
-  @IBOutlet weak var languageCodeLabel: UILabel!
-  
-  @IBOutlet weak var qualityControl: UISegmentedControl!
-
-
-  var voice: AVSpeechSynthesisVoice!
-
-  var speakingText:String?
-
-  func configureView() {
-    let code = voice.language
-
-    let target_locale = NSLocale(localeIdentifier: code)
-    let current_locale = NSLocale.currentLocale()
-
-    title = voice.name
-    languageCodeLabel.text = code
-    languageIdentifierLabel?.text = voice.identifier
-    qualityControl.selectedSegmentIndex = voice.quality.rawValue
-      - AVSpeechSynthesisVoiceQuality.Default.rawValue
     
-    let code_parts = code.componentsSeparatedByString("-")
-    languageNameLabel.text = current_locale.displayNameForKey(NSLocaleLanguageCode, value: code_parts[0])
-    countryNameLabel.text  = current_locale.displayNameForKey(NSLocaleCountryCode, value: code_parts[1])
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+    @IBOutlet weak var languageIdentifierLabel: UILabel?
     
-    let text = target_locale.displayNameForKey(NSLocaleIdentifier, value: code)
-    detailDescriptionLabel.text = text
-    speakingText = text
-  }
-
-
-  override func viewDidAppear(animated: Bool) {
-    super.viewDidAppear(animated)
-
-    if let text = speakingText {
-      let say = AVSpeechUtterance(string: text)
-      say.voice = voice
-      AVSpeechSynthesizer().speakUtterance(say)
+    @IBOutlet weak var languageNameLabel: UILabel!
+    @IBOutlet weak var countryNameLabel: UILabel!
+    @IBOutlet weak var languageCodeLabel: UILabel!
+    
+    @IBOutlet weak var qualityControl: UISegmentedControl!
+    
+    
+    var voice: AVSpeechSynthesisVoice!
+    
+    var speakingText:String?
+    
+    func configureView() {
+        let code = voice.language
+        
+        let target_locale = NSLocale(localeIdentifier: code)
+        let current_locale = NSLocale.currentLocale()
+        
+        title = voice.name
+        languageCodeLabel.text = code
+        languageIdentifierLabel?.text = voice.identifier
+        qualityControl.selectedSegmentIndex = voice.quality.rawValue
+            - AVSpeechSynthesisVoiceQuality.Default.rawValue
+        
+        let code_parts = code.componentsSeparatedByString("-")
+        languageNameLabel.text = current_locale.displayNameForKey(NSLocaleLanguageCode, value: code_parts[0])
+        countryNameLabel.text  = current_locale.displayNameForKey(NSLocaleCountryCode, value: code_parts[1])
+        
+        let text = target_locale.displayNameForKey(NSLocaleIdentifier, value: code)
+        detailDescriptionLabel.text = text
+        speakingText = text
     }
-  }
-  
-  override func viewWillAppear(animated: Bool) {
-    super.viewWillAppear(animated)
-    self.configureView()
-  }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
+    
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if let text = speakingText {
+            let say = AVSpeechUtterance(string: text)
+            say.voice = voice
+            AVSpeechSynthesizer().speakUtterance(say)
+        }
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.configureView()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
 }
 
--- voices-test/MasterViewController.swift
+++ voices-test/MasterViewController.swift
@@ -11,70 +11,95 @@ import AVFoundation
 
 // po (Array(Set(Set(objects.map {$0.language}).map {($0 as NSString).substringToIndex(2)})) as NSArray).description
 
-class MasterViewController: UITableViewController {
-
-  var detailViewController: DetailViewController? = nil
-  let objects = AVSpeechSynthesisVoice.speechVoices()
-  
-  override func viewWillAppear(animated: Bool) {
-    self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
-    super.viewWillAppear(animated)
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-  // MARK: - Segues
-
-  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    if segue.identifier == "showDetail" {
-        if let indexPath = self.tableView.indexPathForSelectedRow {
-
-          let voice = objects[indexPath.row]
-
-          let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
-          
-          controller.voice = voice
-          
-          controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
-          controller.navigationItem.leftItemsSupplementBackButton = true
-      }
-    }
-  }
-
-  // MARK: - Table View
-
-  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-    return 1
-  }
-
-  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return objects.count
-  }
-
-  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-
-    let voice = objects[indexPath.row]
-    cell.textLabel!.text = voice.name
-
-    let locale = NSLocale.currentLocale()
-    cell.detailTextLabel?.text = locale.displayNameForKey(NSLocaleIdentifier, value: voice.language)
+class MasterViewController: UITableViewController, UISearchResultsUpdating {
     
-    return cell
-  }
-
-  override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-    // Return false if you do not want the specified item to be editable.
-    return true
-  }
-
-  override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-
-  }
-
-
+    let all_objects = AVSpeechSynthesisVoice.speechVoices()
+    var objects: [AVSpeechSynthesisVoice]!
+    let locale = NSLocale.currentLocale()
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    func describe(voice: AVSpeechSynthesisVoice) -> String {
+        let code = voice.language
+        return locale.displayNameForKey(NSLocaleIdentifier, value: code) ?? code
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        objects = all_objects
+        
+        tableView.tableHeaderView = searchController.searchBar
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
+        super.viewWillAppear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Segues
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = self.tableView.indexPathForSelectedRow {
+                
+                let voice = objects[indexPath.row]
+                
+                let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
+                
+                controller.voice = voice
+                
+                controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
+    
+    // MARK: - Table View
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return objects.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        
+        let voice = objects[indexPath.row]
+        cell.textLabel!.text = voice.name
+        
+        cell.detailTextLabel?.text = describe(voice)
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        let text = searchController.searchBar.text ?? ""
+        
+        objects = all_objects.filter { voice in
+            text == "" || voice.language.containsString(text) || describe(voice).containsString(text)
+        }
+        
+        tableView.reloadData()
+    }
 }
 
